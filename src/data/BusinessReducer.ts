6607d557e4aa07263264445f8cf4d744bb911fc3
Mathuran Sadagopan <mathuransada@gmail.com>
2024-04-17 00:44:10 -0400

    cleaned up reducer

--- src/data/BusinessReducer.ts
+++ src/data/BusinessReducer.ts
@@ -1,17 +1,7 @@
 import {
-  Account,
-  Business,
   BusinessesData,
   findAllAccountType,
 } from "./Business.js";
-
-class ManageBusiness implements BusinessesData {
-  [key: string]: Business;
-
-  constructor(data: BusinessesData) {
-    Object.assign(this, data);
-  }
-}
 
 export type BusinessesDataActionTypes =
   | "addBusiness"
@@ -28,12 +18,13 @@ export type BusinessesDataAction = {
   accountName?: string;
   newAccountName?: string;
   balance?: number;
+  newBalance?: number;
 };
 
 export function businessDataReducer(
-  draftState: BusinessesData,
+  state: BusinessesData,
   action: BusinessesDataAction,
-) {
+): BusinessesData {
   const {
     type,
     businessName,
@@ -43,51 +34,62 @@ export function businessDataReducer(
     balance,
   } = action;
 
-  if (businessName == undefined) {
-    console.log("Error! BusinessName is undefined.");
-    return draftState;
+  // Validate businessName
+  if (!businessName) {
+    throw new Error("BusinessName is required.");
   }
 
-  if (type == "addBusiness") {
-    const newBusiness = { name: businessName, accounts: [] };
-    draftState[businessName] = newBusiness;
-    return draftState;
-  } else if (type == "deleteBusiness") {
-    const newDraft: BusinessesData = {};
-
-    for (const k of Object.keys(draftState)) {
-      if (k != businessName) {
-        newDraft[k] = draftState[k];
+  switch (type) {
+    case "addBusiness":
+      if (!state[businessName]) {
+        state[businessName] = { name: businessName, accounts: [] };
       }
-    }
-    return newDraft;
-  } else if (type == "editBusinessName") {
-    if (newBusinessName != undefined) {
-      draftState[businessName].name = newBusinessName;
-    }
-  } else if (type == "addAccount") {
-    if (accountName != undefined && balance != undefined) {
-      draftState[businessName].accounts.push({
-        accountName: accountName,
-        balance: balance,
-        types: findAllAccountType(accountName),
-      });
-    }
-  } else if (type == "deleteAccount") {
-    if (action.accountName != undefined) {
-      draftState[action.businessName].accounts = draftState[action.businessName].accounts
-        .filter(
-          (a) => a.accountName != a.accountName,
+      break;
+
+    case "deleteBusiness":
+      delete state[businessName];
+      break;
+
+    case "editBusinessName":
+      if (newBusinessName && state[businessName]) {
+        state[businessName].name = newBusinessName;
+      }
+      break;
+
+    case "addAccount":
+      if (
+        accountName && balance !== undefined &&
+        !state[businessName].accounts.find((a) =>
+          a.accountName === accountName
+        )
+      ) {
+        state[businessName].accounts.push({
+          accountName: accountName,
+          balance: balance,
+          types: findAllAccountType(accountName), // Assuming this function is defined elsewhere
+        });
+      }
+      break;
+
+    case "deleteAccount":
+      if (accountName) {
+        state[businessName].accounts = state[businessName].accounts.filter(
+          (a) => a.accountName !== accountName
         );
-    }
-  } else if (type == "editAccount") {
-    if (accountName != undefined) {
-      draftState[businessName].accounts.forEach((a) => {
-        if (a.accountName == accountName) {
-          a.accountName = newAccountName ?? a.accountName;
-          a.balance = balance ?? a.balance;
+      }
+      break;
+
+    case "editAccount":
+      if (accountName) {
+        const account = state[businessName].accounts.find((a) =>
+          a.accountName === accountName
+        );
+        if (account) {
+          account.accountName = newAccountName ?? account.accountName;
+          account.balance = balance ?? account.balance;
         }
-      });
-    }
+      }
+      break;
   }
+  return state;
 }
