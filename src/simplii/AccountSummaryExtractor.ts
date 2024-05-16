92d1f665f97f6c3491dea4b806ad632c8243fd16
Mathuran Sadagopan <mathuransada@gmail.com>
2024-05-16 17:47:37 -0400

    switched simplii to use xPath object scrapper

--- src/simplii/AccountSummaryExtractor.ts
+++ src/simplii/AccountSummaryExtractor.ts
@@ -1,31 +1,40 @@
-import { Account, findAllAccountType } from "../data/AccountSummaryData.js";
+import {
+  extractAccountValues,
+  XPathMap,
+} from "../content-scripts/ObjectScrapper.js";
+import { Account, AccountDetails } from "../data/AccountSummaryData.js";
 import { AccountSummaryExtractor } from "../data/AccountSummaryExtractor.js";
-import { querySelectNumber, querySelectText } from "../utils.js";
 
 export class SimpliiAccountSummaryExtractor extends AccountSummaryExtractor {
   name = "Simplii";
   type = "Bank";
-
+  
   extractAccountDetails(): Account[] {
-    const accountSummaryList = new Array<Account>();
-    const allAccounts = [
-      document.querySelectorAll(".items__list table.DEPOSIT tr.ember-view"),
-      document.querySelectorAll(".items__list table.CREDIT tr.ember-view"),
-    ];
-
-    allAccounts.forEach((account) =>
-      account?.forEach((row) => {
-        const accountName = querySelectText(row, ".account-name");
-        if (accountName != "") {
-          accountSummaryList.push({
-            accountName: querySelectText(row, ".account-name"),
-            types: findAllAccountType(accountName),
-            balance: querySelectNumber(row, ".balance"),
-          });
-        }
-      }),
+    const accountSummaryList = accountSummaryXPaths.map((xMap) =>
+    extractAccountValues(xMap) as Account
     );
-
+    
     return accountSummaryList;
   }
 }
+
+const accountSummaryXPaths: XPathMap<AccountDetails>[] = [
+  {
+    accountName:
+      "/html/body/div[6]/div/div[2]/div[2]/main/div/div/div[2]/div[1]/div[3]/section/section[3]/div/div[1]/table/tbody/tr[2]/td[1]/div/a",
+    balance:
+      "/html/body/div[6]/div/div[2]/div[2]/main/div/div/div[2]/div[1]/div[3]/section/section[3]/div/div[1]/table/tbody/tr[2]/td[2]/span",
+  },
+  {
+    accountName:
+      "/html/body/div[6]/div/div[2]/div[2]/main/div/div/div[2]/div[1]/div[3]/section/section[3]/div/div[1]/table/tbody/tr[3]/td[1]/div/a",
+    balance:
+      "/html/body/div[6]/div/div[2]/div[2]/main/div/div/div[2]/div[1]/div[3]/section/section[3]/div/div[1]/table/tbody/tr[3]/td[2]/span",
+  },
+  {
+    accountName:
+      "/html/body/div[6]/div/div[2]/div[2]/main/div/div/div[2]/div[1]/div[3]/section/section[3]/div/div[2]/table/tbody/tr[2]/td[1]/div/a",
+    balance:
+      "/html/body/div[6]/div/div[2]/div[2]/main/div/div/div[2]/div[1]/div[3]/section/section[3]/div/div[2]/table/tbody/tr[2]/td[3]/span",
+  },
+];
--- src/simplii/content.ts
+++ src/simplii/content.ts
@@ -1,5 +1,3 @@
-/// <reference lib="dom" />
-import getXPath from "get-xpath";
 import { SimpliiAccountSummaryExtractor } from "./AccountSummaryExtractor.js";
 import { logCreditCardData } from "./creditCardsAccounts.js";
 import { logDepositAccountsData } from "./depositsAccounts.js";
@@ -26,30 +24,3 @@ function locationHashChanged() {
 locationHashChanged();
 
 window.onhashchange = locationHashChanged;
-
-////////////////////////////////////////////////////////////////////////
-
-document.addEventListener("click", (event) => {
-  const target = event.target as HTMLElement;
-  const path = getXPath(target)
-  // const elem = target.getPath()
-  console.log({target, path});
-  const findElemAgain = getElementByXpath(path)
-  console.log("Finding elem using path", findElemAgain)
-
-  event.preventDefault(); // Prevent default actions for better visualization
-  event.stopPropagation(); // Stop the event from bubbling up
-}, true); // Use capturing instead of bubbling
-
-document.addEventListener("mouseover", (event) => {
-  event.target.style.outline = "2px solid red";
-});
-
-document.addEventListener("mouseout", (event) => {
-  event.target.style.outline = "";
-});
-
-
-function getElementByXpath(path: string) {
-  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
